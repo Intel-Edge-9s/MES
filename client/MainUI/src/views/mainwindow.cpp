96a05f8a4aa0b3096aff249cd559cbd7c53c2775
Hyung Jun An <https://github.com/Intel-Edge-9s-MES/MES>
2026-03-05 18:46:42 +0900

    통신기능 추가했습니다 데이터 알아서 가져가면서 하세요

--- client/MainUI/src/views/mainwindow.cpp
+++ client/MainUI/src/views/mainwindow.cpp
@@ -9,14 +9,73 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    //===========================================
+    //
+    //===========================================
+    ua = new OpcUaService(this);
+    //===========================================
+    //
+    //===========================================
 
     setupNavigation();
     moveToPage(PageType::Login);
+    //===========================================
+    //
+    //===========================================
+    auto* login = qobject_cast<LoginWidget*>(ui->loginPage);
+    if (login) {
+        // ✅ 로그인 성공 “딱 한번”만 통신 시작
+        connect(login, &LoginWidget::loginSuccess, this, [this](){
+            startOpcUaOnce();
+        });
+        connect(ua, &OpcUaService::mfgTempUpdated, this, [](double t){
+            qDebug() << "MFG TEMP =" << t;
+        });
+
+        connect(ua, &OpcUaService::logTempUpdated, this, [](double t){
+            qDebug() << "LOG TEMP =" << t;
+        });
+
+    }
+
+    //===========================================
+    //
+    //===========================================
+
 }
 
 MainWindow::~MainWindow() {
     delete ui;
 }
+//===========================================
+//
+//===========================================
+void MainWindow::startOpcUaOnce()
+{
+    if (uaStarted) return;
+    uaStarted = true;
+
+    ua->connectMfg(
+        "opc.tcp://10.10.16.208:4850",
+        "mes","mespw_change_me",
+        "/home/pi/opcua_project/certs/mes/cert.der",
+        "/home/pi/opcua_project/certs/mes/key.der",
+        "/home/pi/opcua_project/certs/mes/trust_mfg.der"
+        );
+
+    ua->connectLog(
+        "opc.tcp://10.10.16.210:4841",
+        "mes","mespw_change_me",
+        "/home/pi/opcua_project/certs/mes/cert.der",
+        "/home/pi/opcua_project/certs/mes/key.der",
+        "/home/pi/opcua_project/certs/mes/trust_log.der"
+        );
+
+
+}
+//===========================================
+//
+//===========================================
 
 void MainWindow::setupNavigation() {
     // 위젯 캐스팅
