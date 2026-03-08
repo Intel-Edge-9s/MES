c63dc2f0dbe52fd797560b243059fae3f172c4d8
bhg <anysst11@gmail.com>
2026-03-08 12:00:06 +0900

    에러로그 query 제거

--- client/MainUI/src/services/environment_logs_service.h
+++ client/MainUI/src/services/environment_logs_service.h
@@ -1,28 +1,10 @@
 #ifndef ENVIRONMENT_LOGS_SERVICE_H
 #define ENVIRONMENT_LOGS_SERVICE_H
 
-#include <QVector>
-#include <QDateTime>
-#include <QString>
-
-struct LogEntry
-{
-    QString time;
-    QString line;
-    QString type;
-    QString message;
-    QDateTime eventAt;
-};
-
 class EnvironmentLogsService
 {
 public:
-    QVector<LogEntry> fetchRecentLogs();
-    QVector<LogEntry> fetchLogsAfter(const QDateTime &lastEventAt);
-
-private:
-    QString resolveLine(const QString &processId) const;
-    QString resolveType(const QString &desc) const;
+    EnvironmentLogsService();
 };
 
-#endif // ENVIRONMENT_LOGS_SERVICE_H
+#endif
